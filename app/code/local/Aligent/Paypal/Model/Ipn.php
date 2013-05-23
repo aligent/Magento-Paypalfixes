b57e8455214f53caa0226b177cfaaf90a6113b82
Jim O'Halloran <jim@aligent.com.au>
2013-05-23 16:11:32 +0930

    Add cpmment to the original order and raise an admin notification whenever a credit IPN is received.

--- app/code/local/Aligent/Paypal/Model/Ipn.php
+++ app/code/local/Aligent/Paypal/Model/Ipn.php
@@ -13,7 +13,7 @@ class Aligent_Paypal_Model_Ipn extends Mage_Paypal_Model_Ipn {
      * Process a refund or a chargeback
      */
     protected function _registerPaymentRefund() {
-		if (Mage::getStoreConfig(self::CONFIG_IPN_REFUND_METHOD) == Aligent_Paypal_Model_System_Config_Source_Refundmethod::METHOD_DEFAULT) {
+        if (Mage::getStoreConfig(self::CONFIG_IPN_REFUND_METHOD) == Aligent_Paypal_Model_System_Config_Source_Refundmethod::METHOD_DEFAULT) {
             return parent::_registerPaymentRefund();
         } else {
 
@@ -24,6 +24,20 @@ class Aligent_Paypal_Model_Ipn extends Mage_Paypal_Model_Ipn {
 
             Mage::log('IPN Refund received.  Reason Code: '.$reason.' isRefundFinal: '.$isRefundFinal.' Amount: '.$amount);
 
+            $vCommentText = Mage::helper('paypal')->__('Refunded amount of %s. Transaction ID: "%s".', $this->_order->getBaseCurrency()->formatTxt($amount), $this->getRequestData('txn_id'));
+            $vCommentText .= ' Reason: '.$this->_info->explainReasonCode($reason);
+
+            $this->_createIpnComment($vCommentText, true);
+            $this->_order->save();
+
+            $vCommentText .= " for order #".$this->_order->getIncrementId();
+
+            $oNotification = Mage::getModel('adminnotification/inbox');
+            $oNotification->setSeverity(Mage_AdminNotification_Model_Inbox::SEVERITY_MINOR);
+            $oNotification->setDateAdded(date("c", time()));
+            $oNotification->setTitle('Paypal Refund IPN Received');
+            $oNotification->setDescription($vCommentText);
+            $oNotification->save();
 
         }
 
